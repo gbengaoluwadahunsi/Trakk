47ad4e76cc41a7b9321b542e7ef281f506009fdd
Gbenga Oluwadahunsi <gbengaoluwadahunsi01@gmail.com>
2024-05-23 14:49:15 +0800

    Update: some formatting and linting of the pages

--- src/app/employee/login/page.tsx
+++ src/app/employee/login/page.tsx
@@ -4,13 +4,13 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { useForm } from "react-hook-form";
-import { Poller_One }  from "next/font/google";
+import { Poller_One } from "next/font/google";
 
 export const poller_one = Poller_One({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: "400"
-})
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
 
 const LoginPage = () => {
   const {
@@ -45,9 +45,11 @@ const LoginPage = () => {
   return (
     <div className="flex  flex-col items-center justify-center h-screen bg-gray-100">
       <div className="bg-slate-800 p-12 rounded shadow-md w-full max-w-md">
-      <span className={` text-2xl md:text-4xl flex justify-center items-center py-8 font-mono font-extrabold  ${poller_one.className} text-zinc-200`}>
+        <span
+          className={` text-2xl md:text-4xl flex justify-center items-center py-8 font-mono font-extrabold  ${poller_one.className} text-zinc-200`}
+        >
           Trakk
-        </span >
+        </span>
         <h1 className=" text-zinc-100 text-2xl font-bold">Employee Login</h1>
         <form onSubmit={handleSubmit(onSubmit)} className="py-4 space-y-6">
           <input
